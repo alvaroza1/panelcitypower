dc5475727fb50f05a24e0d08e01ab3146f48edfc
Alvaro Zetina <alvaro_za1@hotmail.com>
2015-02-21 21:45:32 -0600

    Initial
    Proyecto

---
+++ app/views/ejercicios/ejercicio1.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Ejercicio1</title>
+    {{ HTML::style('resources/css/styles.css'); }}
+    {{ HTML::script('resources/js/jquery.js') }}
+    {{ HTML::script('resources/js/functions.js') }}
+</head>
+<body>
+	<center><h1>Datos Generales</h1></center>
+	<center>
+		<p >
+			<strong>
+				Nombre: Alvaro<br>
+				Registro: 2363295<br>
+				Carrera: Ingeniería de Software <br>
+				Sexo: Masculino <br>
+				Edad: 21 <br>
+				Escuela: Universidad Autonoma de Guadalajara <br>
+			</strong>
+		</p>
+		
+		<p >
+			<ul>	
+				<li>Futbol</li>
+				<li>Estudiar</li>
+				<li>Trabajar</li>
+				<li>Programar</li>
+				<li>Correr</li>
+				<li>Videojuegos</li>
+			</ul>
+		</p>
+	</center>
+</body>
+</html>
